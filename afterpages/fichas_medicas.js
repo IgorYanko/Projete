07d8816fb3cb8bb500bdbed7a365925728877044
IgorYanko <igoryanko@hotmail.com>
2022-07-05 20:21:28 -0300

    first commit

---
+++ afterpages/fichas_medicas.js
@@ -0,0 +1,3 @@
+function clicar() {
+  window.location = "./Gabriel.png";
+}
---
+++ inicio.js
@@ -0,0 +1,7 @@
+function clicar1() {
+  window.location = "./afterpages/fichas_medicas.html";
+}
+
+function clicar2() {
+  window.location = "./afterpages/pacientes_ativos.html";
+}
