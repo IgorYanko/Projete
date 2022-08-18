2d1ce63ec362c93ac889dc7830bc238312364404
IgorYanko <igoryanko@hotmail.com>
2022-08-17 23:30:16 -0300

    finalizei a parte do cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,10 @@
+function enviarDados() {
+  var nome = document.getElementById("nome").value;
+  var idade = document.getElementById("data").value;
+  var peso = document.getElementById("peso").value;
+  var altura = document.getElementById("altura").value;
+  var sexo = document.getElementById("sexo").value;
+  var alergias = document.getElementById("alergias").value;
+  var historico = document.getElementById("historico").value;
+  alert(nome, data, peso, altura, sexo, alergias, historico);
+}
