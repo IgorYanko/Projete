1587630ee6285fb72bc1632ca8fc00db4c207871
IgorYanko <igoryanko@hotmail.com>
2022-08-17 22:11:34 -0300

    adicionei a parte do cadastro

--- inicio.js
+++
@@ -1,7 +0,0 @@
-function clicar1() {
-  window.location = "./fichas_medicas";
-}
-
-function clicar2() {
-  window.location = "./afterpages/pacientes_ativos.html";
-}
---
+++ pagina _inicial/inicio.js
@@ -0,0 +1,11 @@
+function clicar1() {
+  window.location = "./afterpages/fichas_medicas.html";
+}
+
+function clicar2() {
+  window.location = "./afterpages/pacientes_ativos.html";
+}
+
+function clicar3() {
+  window.location = "./afterpages/cadastro.html";
+}
