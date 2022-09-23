b8ff7b82e42f469623411e49d3acd29c8a8d2631
IgorYanko <igoryanko@hotmail.com>
2022-09-23 16:56:26 -0300

    Quiz começado

--- questionario/questionario.js
+++ questionario/questionario.js
@@ -1,1 +1,117 @@
-a
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta = document.querySelector('#pergunta')
+
+let a = document.querySelector()
+let b = document.querySelector()
+
+const q0 = {
+    numQuestao: 0;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+
+const q1 = {
+    numQuestao: 1;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+
+const q2 = {
+    numQuestao: 2;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+
+const q3 = {
+    numQuestao: 3;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+
+const q4 = {
+    numQuestao: 4;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q5 = {
+    numQuestao: 5;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q6 = {
+    numQuestao: 6;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q7 = {
+    numQuestao: 7;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q8 = {
+    numQuestao: 8;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q9 = {
+    numQuestao: 9;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q10 = {
+    numQuestao: 10;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q11 = {
+    numQuestao: 11;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q12 = {
+    numQuestao: 12;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
+    
+const q13 = {
+    numQuestao: 13;
+    pergunta: "Pergunta",
+    alternativa1: "Sim",
+    alternativa2: "Nao",
+    exludentes: "",
+}
